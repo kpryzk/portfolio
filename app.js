8d2f16ef348eaa34cd4f7c8841cd19410347afc9
kpryzk <kepryzk@gmail.com>
2017-07-11 22:23:08 -0700

    added jquery constructor function, css needs work

--- app.js
+++ app.js
@@ -2,9 +2,29 @@
 
 Portfolio.all = [];
 
-function Portfolio(name, image, path) {
-  this.name = name;
-  this.image = image;
-  this.path = path;
-  Portfolio.all.push(this);
+function Portfolio(rawDataObj) {
+  this.title = rawDataObj.title;
+  this.image = rawDataObj.image;
+  this.path = rawDataObj.path;
+  // this.publishedOn = rawDataObj.publishedOn;
 }
+
+Portfolio.prototype.toHtml = function() {
+  var $newPortfolio = $('article.template').clone();
+
+  $newPortfolio.removeClass('template')
+
+  $newPortfolio.find('h6').html(this.title);
+  $newPortfolio.find('a').attr('href', this.path);
+  $newPortfolio.find('img').attr('src', this.image);
+
+  return $newPortfolio;
+};
+
+rawData.forEach(function(articleObject) {
+  Portfolio.all.push(new Portfolio(articleObject));
+});
+
+Portfolio.all.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
